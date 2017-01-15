a583a9468bf564ebf74c292cfc732792ef04ecb2
Josselin Auguste <jauguste@iblop.net>
2017-01-15 20:41:43 +0100

    Fix syntaxic colorization for non-ascii characters

--- sample-code/SimpleBindings.fs
+++ sample-code/SimpleBindings.fs
@@ -10,7 +10,7 @@ let c_byte       = 'F'B
 
 let a_int16      = 44s
 let a_uint16     = 44us
-let a_int        = 100 
+let a_int        = 100
 let a_int32      = 100l
 let a_uint       = 100u
 let a_uin32      = 100ul
@@ -29,8 +29,8 @@ let h_float32    = 180.3E+32F
 let i_float32    = 180.4e+10F
 
 let a_float64    = 30.0
-let b_float64    = 40.0e5 
-let c_float64    = 2.3E+32  
+let b_float64    = 40.0e5
+let c_float64    = 2.3E+32
 let d_float64    = 2.3e+32
 let e_float64    = 0x0000000000000000LF
 
@@ -39,6 +39,7 @@ let a_bigint     = 304554I
 
 let binding = "empty"
 let aString : string = "typed"
+let accentué = "accentué"
 
 let tupA1, tupA2 = 1, 2
 let (tupB1, tupB2) : decimal * decimal = 40m, 50M
