9d241210bec0794cf4bc1902669b9cb92574f9c4
Maxime Mangel <maxime.shadaen@protonmail.com>
2018-05-24 22:18:54 +0200

    Improve type colorization (#49)
    * Fix #36: Keyword+apostrohpe names in match expressions are colored as keywords
    
    * Fix #45, Fix #44: improve type colorization
    
    * Update lock file for latest npm5 version
    
    * Fix name conflict

--- sample-code/SimpleTypes.fs
+++ sample-code/SimpleTypes.fs
@@ -20,11 +20,20 @@ type Class1() =
     member this.X = "F#"
 
 
-type FancyClass(thing:int) as xxx =
+type FancyClass(thing:int, var2 : string) as xxx =
 
     let pf() = xxx.Test()
 
     member xxx.Test() = "F#"
+
+type FancyClass1(?thing:int) =
+    class end
+
+type private FancyClass2 (?thing:int) =
+    class end
+
+type FancyClass3 private (?thing:int) =
+    class end
 
 module test =
     let t = 1
@@ -44,7 +53,7 @@ let test match' =
     | CaseA -> ""
     | CaseB -> ""
 
-let test return' =
+let test2 return' =
     match return' with
     | CaseA -> ""
     | CaseB -> ""
